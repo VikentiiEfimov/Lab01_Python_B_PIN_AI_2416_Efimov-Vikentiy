a1d6ceb9936984e24b13aa58d88d79c94d0bae9d
VikentiiEfimov <efimowikenty@yandex.ru>
2026-02-18 23:22:20 +0300

    thendfilm

--- parser.py
+++ parser.py
@@ -10,6 +10,7 @@ os.makedirs("dataset/good", exist_ok=True)
 os.makedirs("dataset/bad", exist_ok=True)
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument('--headless')
 options.page_load_strategy = 'eager'
 driver = webdriver.Chrome(options=options)
 
@@ -19,7 +20,6 @@ good_counter = 0
 bad_counter = 0
 MAX_REVIEWS = 999
 per_options = [10, 25, 50, 75, 100, 200]
-wait = WebDriverWait(driver, 5)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -36,52 +36,59 @@ def save_review(title, text, film_name, status, pn_counter, counter):
     path = os.path.join("dataset", folder, filename)
     with open(path, "w", encoding="utf-8") as f:
         f.write(film_name + "\n\n")
-        if title != "": f.write(title + "\n\n")
+        if title != "": 
+            f.write(title + "\n\n")
         f.write(text)
     logging.info(f"Сохранён {counter+1}-й {status} отзыв: {filename} по {film_name}")
+
 
 for film_id, film_name, pos_rew, neg_rew in films:
     for status in ("good", "bad"):
         target = int(int(pos_rew) * 0.084) + 1 if status == "good" else int(neg_rew)
         perpage = next((x for x in per_options if x >= target), 200)
         counter = 0
+    
+        url = f"https://www.kinopoisk.ru/film/{film_id}/reviews/ord/rating/status/{status}/perpage/{perpage}/page/1/"
+        driver.get(url)
 
-        while counter < target:
-            url = f"https://www.kinopoisk.ru/film/{film_id}/reviews/ord/rating/status/{status}/perpage/{perpage}/page/1/"
-            driver.get(url)
-
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "reviewItem")))
+        except:
             try:
+                captcha_button = WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, "input.CheckboxCaptcha-Button")))
+                captcha_button.click()
                 WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, "reviewItem"))
-                )
+                    EC.presence_of_element_located((By.CLASS_NAME, "reviewItem")))
+                logging.info(f"Капча пройдена")
             except:
+                logging.info(f"Нет отзывов")
                 break
 
-            reviews = driver.find_elements(By.CLASS_NAME, "reviewItem")
+        reviews = driver.find_elements(By.CLASS_NAME, "reviewItem")
 
-            for review in reviews:
-                if counter >= target:
+        for review in reviews:
+            if counter >= target:
+                break
+
+            title = review.find_element(By.CLASS_NAME, "sub_title").text.strip()
+            text_element = review.find_element(By.CLASS_NAME, "brand_words")
+            text = text_element.text.strip()
+
+            if status == "good":
+                save_review(title, text, film_name, status, good_counter, counter)
+                if good_counter == MAX_REVIEWS:
                     break
+                else: good_counter += 1
 
-                title = review.find_element(By.CLASS_NAME, "sub_title").text.strip()
-                text_element = review.find_element(By.CLASS_NAME, "brand_words")
-                text = text_element.text.strip()
+            else:
+                save_review(title, text, film_name, status, bad_counter, counter)
+                if bad_counter == MAX_REVIEWS:
+                    break
+                else: bad_counter += 1
 
-                if status == "good":
-                    save_review(title, text, film_name, status, good_counter, counter)
-                    if good_counter == MAX_REVIEWS:
-                        counter = target +1
-                        break
-                    else: good_counter += 1
-
-                else:
-                    save_review(title, text, film_name, status, bad_counter, counter)
-                    if bad_counter == MAX_REVIEWS:
-                        counter = target +1
-                        break
-                    else: bad_counter += 1
-
-                counter += 1
+            counter += 1
 
 driver.quit()
 logging.info(f"Готово! Положительных: {good_counter+1}, отрицательных: {bad_counter+1}.")
--- stschyot.py
+++ stschyot.py
@@ -24,28 +24,5 @@ with open(path, "r", encoding="utf-8") as f:
 #print(f"{pos_k}~{proz} - {neg_k} = {raz}")
 
 
-"""perpa = [x - (int(int(rew) * 0.084) + 1) for x in per]
-    #print(x for x in perpa)
-    ags = zip(per, perpa)
-    perpaz = min([abs(r) for v, r in ags])
-    vsego = (v for v, r in ags if r == perpaz)
-    #print(x for x in vsego)
-    print(f"~ {perpaz}")
-    mni = 1000
-    perpage = 10
-    for x in per:
-        for y in per:
-            if x <= y:
-                perpa = x +y - (int(int(rew) * 0.084) + 1)
-                #print(f"{perpa} == {x +y}")
-                if abs(perpa) < mni:
-                    mni = abs(perpa)
-                    perpage = x +y
-                    print(f"{mni} == {x} + {y}")
-    #if perpage - (int(int(rew) * 0.084) + 1) < 0:
-    print(f"##### {n}: {(int(int(rew) * 0.084) + 1)} ~ {perpage}")
-    n+=1"""
-
-        
 def get_films():
     return zip(film_id, film_name, pos_rew, neg_rew)
